a7bfc2227594f86cccee5b46889dad24604de2ca
gaplin <gaplinn@gmail.com>
2024-01-21 19:21:56 +0100

    10

---
+++ 10/day10p2.py
@@ -0,0 +1,22 @@
+def step(sequence: list) -> list:
+    current_value = None
+    current_count = 0
+    result = []
+    for char in sequence:
+        if char == current_value:
+            current_count += 1
+        else:
+            if current_value != None:
+                result += [str(current_count), current_value]
+            current_value = char
+            current_count = 1
+    else:
+        result += [str(current_count), current_value]
+
+    return result
+
+seq = [x for x in open('input2.txt').read().strip()]
+for _ in range(50):
+    seq = step(seq)
+
+print(len(seq))
